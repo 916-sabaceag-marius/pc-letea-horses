836578244d36c1b5c5cf5192aa0b8bd7cd1056e6
ANRazvan <alexe.razvan05@gmail.com>
2025-12-10 20:30:10 +0200

    fixed issue when getting a product by an unlogged user

--- Honse/Honse.Managers/OrderManager.cs
+++ Honse/Honse.Managers/OrderManager.cs
@@ -196,7 +196,7 @@ namespace Honse.Managers
 
             foreach (var item in request.Products)
             {
-                var product = await _productResource.GetByIdNoTracking(item.ProductId, userId ?? Guid.Empty);
+                var product = await _productResource.GetProductByIdPublic(item.ProductId);
                 if (product == null)
                     throw new ValidationException($"Product with ID {item.ProductId} not found!");
 
